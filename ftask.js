ee05b0ada03f97646bacb954b45230e39298c1fa
BesanNoor <sfbeno520@gmail.com>
2025-11-01 20:17:33 +0200

    Add files via upload

---
+++ ftask.js
@@ -0,0 +1,10 @@
+//ביסאם פרח 211861208
+//נור צובח 214459463
+
+const num = 30
+const d1 = num % 2 === 0
+const d2 = num % 3 === 0
+const d3 = num % 5 === 0
+
+const result = d1 + d2 + d3
+console.log (result);
